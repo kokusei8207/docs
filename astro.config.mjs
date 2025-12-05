193679ad4c492c9ac00059864aed5130d664e8aa
Rizky Pratama <104710750+Aruh1@users.noreply.github.com>
2025-12-05 17:37:10 +0000

    feat: add initial content configuration and documentation
    - Created content configuration file to define collections using Astro's content module.
    - Added an example guide in Markdown format to demonstrate guide structure.
    - Introduced main documentation index with a welcome message and hero image.
    - Added a localization reference for terms used in the context of Isekai genre, including translations and notes.
    - Configured TypeScript settings for the project.

---
+++ astro.config.mjs
@@ -0,0 +1,65 @@
+// @ts-check
+
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: "Docs",
+			logo: {
+				// src: './src/assets/logo.svg',
+				replacesTitle: true,
+				light: "./src/assets/logo.svg",
+				dark: "./src/assets/logo-dark.svg",
+			},
+			social: [
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/yuramedia/",
+				},
+				{
+					icon: "facebook",
+					label: "Facebook",
+					href: "https://www.facebook.com/yuramedialink",
+				},
+				{
+					icon: "instagram",
+					label: "Instagram",
+					href: "https://www.instagram.com/yuramedialink/",
+				},
+				{
+					icon: "linkedin",
+					label: "LinkedIn",
+					href: "https://www.linkedin.com/company/yuramedialink",
+				},
+				{
+					icon: "tiktok",
+					label: "TikTok",
+					href: "https://www.tiktok.com/@yuramedialink",
+				},
+				{
+					icon: "x.com",
+					label: "Twitter",
+					href: "https://x.com/yuramedialink",
+				},
+			],
+			sidebar: [
+				{
+					label: "Guides",
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: "Example Guide", slug: "guides/example" },
+					],
+				},
+				{
+					label: "Referensi",
+					autogenerate: { directory: "reference" },
+				},
+			],
+			tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 2 },
+		}),
+	],
+});
